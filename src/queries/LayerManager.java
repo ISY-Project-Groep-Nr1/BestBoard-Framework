1b1db24738ca641a4b3e0146b95c6432949e641e
AlexDGD <alexmendelts@hotmail.com>
2025-09-18 11:27:16 +0200

    Add LayerManager

---
+++ src/queries/LayerManager.java
@@ -0,0 +1,38 @@
+public class LayerManager {
+    // adds a new layer
+    public void addLayer(String name, Object data, boolean on) {
+
+    }
+
+    // deletes a layer
+    public void deleteLayer(String name) {
+
+    }
+
+    //
+    public Layer getLayer(String name) {
+        return ...
+    }
+
+
+    public void enableLayer(String name) {
+
+    }
+
+
+    public void disableLayer(String name) {
+
+    }
+
+
+    public void toggleLayer(String name) {
+        if (layer.enabled == false) {
+            LayerManager.enableLayer
+        }
+    }
+
+
+    public Layer getAllActive() {
+
+    }
+}
