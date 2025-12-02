84e5e7e7d55a64a2edb28504f2ffe07a0aacf81c
Pixel5908 <ruben.thie@hotmail.com>
2025-12-02 13:39:26 +0100

    removed encryption as it is not important and is bloaty

--- src/com/nr1/SaveManager.java
+++ src/com/nr1/SaveManager.java
@@ -1,99 +1,38 @@
 package com.nr1;
 
 import javax.crypto.*;
-import javax.crypto.spec.GCMParameterSpec;
 import java.io.*;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.util.Base64;
 
 public class SaveManager {
     private String saveLocation;
-    private final SecretKey KEY = generateKey(128);
-    private final GCMParameterSpec I_V = generateIv();
-    private final String ALGORITHM = "AES/GCM/NoPadding";
 
 
-    public SaveManager() throws NoSuchAlgorithmException {
+    public SaveManager() {
         this.saveLocation = "save.txt";
     }
 
-    public SaveManager(String saveLocation) throws NoSuchAlgorithmException {
+    public SaveManager(String saveLocation) {
         this.saveLocation = saveLocation;
-//        this.createSaveFile();
-        System.out.println(KEY);
-        System.out.println(I_V);
     }
-
-//    public boolean createSaveFile() {
-//        try {
-//            File myObj = new File(saveLocation);
-//            if (myObj.createNewFile()) {
-//                System.out.println("File created: " + myObj.getName());
-//                return true;
-//            } else {
-//                System.out.println("File already exists.");
-//            }
-//        } catch (IOException e) {
-//            System.out.println("An error occurred.");
-//            e.printStackTrace();
-//        }
-//        return false;
-//    }
-
 
 
     public boolean save(char[][] currentBoard, String activePlayer) {
         try {
-            String plainText = generatePlainText(currentBoard, activePlayer);
-            String encryptedText = encrypt(ALGORITHM, plainText, KEY, I_V);
-
-            String decryptedText = decrypt(ALGORITHM, encryptedText, KEY, I_V);
+            String data = generateSaveData(currentBoard, activePlayer);
 
             FileWriter myWriter = new FileWriter("save.txt");
-            myWriter.write("Encrypted text: " +
-                    "" + encryptedText);
-
-            myWriter.write("\n" + "Plain text: " + decryptedText);
+            myWriter.write(data);
             myWriter.close();
             System.out.println("Successfully wrote to the file.");
             return true;
-        } catch (IOException | NoSuchAlgorithmException | NoSuchPaddingException | InvalidAlgorithmParameterException |
-                 InvalidKeyException | BadPaddingException | IllegalBlockSizeException e) {
+        } catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
         return false;
     }
 
-
-
-    private String encrypt(String algorithm, String input, SecretKey key, GCMParameterSpec iv)
-            throws NoSuchPaddingException, NoSuchAlgorithmException,
-            InvalidAlgorithmParameterException, InvalidKeyException,
-            BadPaddingException, IllegalBlockSizeException {
-
-        Cipher cipher = Cipher.getInstance(algorithm);
-        cipher.init(Cipher.ENCRYPT_MODE, key, iv);
-        byte[] cipherText = cipher.doFinal(input.getBytes());
-        return Base64.getEncoder().encodeToString(cipherText);
-    }
-
-    public static String decrypt(String algorithm, String cipherText, SecretKey key, GCMParameterSpec iv)
-            throws NoSuchPaddingException, NoSuchAlgorithmException,
-            InvalidAlgorithmParameterException, InvalidKeyException,
-            BadPaddingException, IllegalBlockSizeException {
-
-        Cipher cipher = Cipher.getInstance(algorithm);
-        cipher.init(Cipher.DECRYPT_MODE, key, iv);
-        byte[] plainText = cipher.doFinal(Base64.getDecoder().decode(cipherText));
-        return new String(plainText);
-    }
-
-    private String generatePlainText(char[][] currentBoard, String activePlayer) {
-        // the encryption method is AES/GCM
+    private String generateSaveData(char[][] currentBoard, String activePlayer) {
         StringBuilder plainText = new StringBuilder();
         plainText.append(currentBoard.length + ":" + currentBoard[0].length + ";");
         for (char[] line : currentBoard) {
@@ -104,20 +43,7 @@ public class SaveManager {
         return plainText.toString();
     }
 
-    private SecretKey generateKey(int n) throws NoSuchAlgorithmException {
-        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
-        keyGenerator.init(n);
-        SecretKey key = keyGenerator.generateKey();
-        return key;
-    }
-
-    private GCMParameterSpec generateIv() {
-        byte[] iv = new byte[12];
-        new SecureRandom().nextBytes(iv);
-        return new GCMParameterSpec(128, iv);
-    }
-
-    static void main() throws NoSuchAlgorithmException {
+    static void main() {
         char[][] test = {{'a','b','c'},{'1','2','3'},{'!','@','#'}};
         SaveManager saveManager = new SaveManager();
         saveManager.save(test, "wow");
